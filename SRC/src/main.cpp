1a559453f4a63f8969c95a6be95f9e3d7c09bb34
HellZDarkArkon <maximumwerewolf@gmail.com>
2021-11-26 22:01:31 -0500

    setup
    SDL setup

--- SRC/src/main.cpp
+++ SRC/src/main.cpp
@@ -1,9 +1,13 @@
 /// Metroidvania main.cpp ///
 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 
 
-int main()
+int main(int argc, char* args[])
 {
+	std::cout<<"Yeah"<<std::endl;
+
 	return 0;
 }
